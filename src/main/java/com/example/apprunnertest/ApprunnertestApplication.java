4da845096cf70fdcb46205cadb918d79882a571a
kimin <kiminchan12341@gmail.com>
2023-05-08 16:33:07 +0900

    Initial commit

---
+++ src/main/java/com/example/apprunnertest/ApprunnertestApplication.java
@@ -0,0 +1,13 @@
+package com.example.apprunnertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprunnertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprunnertestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apprunnertest/ApprunnertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apprunnertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprunnertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
